e4f7230b1ae9782de555892a46845e893bed179a
Kratika Kaushik <kratikakaushik27@gmail.com>
2022-07-08 16:53:17 +0530

    Fourth commit

--- app.js
+++ app.js
@@ -125,10 +125,10 @@ app.get("/:name",function(req,res){
   });
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+let port = process.env.PORT||3000;
+// if (port == null || port == "") {
+//   port = 3000;
+// }
 
 app.listen(port, function() {
   console.log("Server started on port successfully");
