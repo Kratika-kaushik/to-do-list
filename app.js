40ced6979f0d29fd197c0656b0284f67f805fc3f
Kratika Kaushik <kratikakaushik27@gmail.com>
2022-07-08 16:33:24 +0530

    Add gitignore,Procfile, update ports

--- app.js
+++ app.js
@@ -123,14 +123,13 @@ app.get("/:name",function(req,res){
 
     }
   });
-
-
-
-
-
-
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started on port successfully");
 });
